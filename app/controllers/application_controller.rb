085b93e589047fe97aa9389c873291b512dcc42b
logan <rjsnh1522@gmail.com>
2017-05-25 14:47:42 +0530

    some changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,43 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  layout :layout_by_resource
+  
+#   before_action :configure_permitted_parameters,if: :devise_controller?
+
+#   protected
+
+#   def configure_permitted_parameters
+
+
+#     devise_parameter_sanitizer.for(:sign_up) {|u| 
+#     u.permit(:first_name,:last_name,:username,:phone,:email,:password,:remember_me)
+#   }
+
+
+
+#  devise_parameter_sanitizer.for(:sign_in) {|u| 
+#     u.permit(:email,:password)
+#   }
+
+#   end
+  
+#  devise_parameter_sanitizer.for(:account_update) {|u| 
+#     u.permit(:first_name,:last_name,:username,:phone,:email,:password,:current_password,:remember_me)
+#   }
+
+
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :expenses
+  devise_scope :user do
+  root to: "devise/sessions#new"
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
